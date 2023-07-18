aa18a7029e31778e666678513f86a6d437b675d5
Jojhan Silva <silvajojhan@gmail.com>
2023-07-18 15:52:44 -0500

    env port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs')
 const path = require('path')
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
