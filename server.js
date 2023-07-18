10c7c8642693f9376ab6922f4b8ce2bcd9addeac
Jojhan Silva <silvajojhan@gmail.com>
2023-07-18 15:06:56 -0500

    added get, post, and delete routes

--- server.js
+++ server.js
@@ -14,6 +14,40 @@ app.use(express.static('public'));
 // Path to the JSON database file
 const dbPath = path.join(__dirname, 'db', 'db.json');
 
+// Get all notes
+app.get('/api/notes', (req, res) => {
+    const notes = readNotesFromDB();
+    res.json(notes);
+  });
+  
+  // Create a new note
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = readNotesFromDB();
+    newNote.id = Date.now().toString(); // Generate a unique ID for the note
+  
+    notes.push(newNote);
+    writeNotesToDB(notes);
+  
+    res.status(201).json(newNote);
+  });
+  
+  // Delete a note
+  app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const notes = readNotesFromDB();
+    const index = notes.findIndex((note) => note.id === noteId);
+  
+    if (index !== -1) {
+      notes.splice(index, 1);
+      writeNotesToDB(notes);
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+  
+
 
 // Start the server
 app.listen(port, () => {
