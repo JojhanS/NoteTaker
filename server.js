726601f797c41f974eef0dbe56c425c694633041
Jojhan Silva <silvajojhan@gmail.com>
2023-07-18 14:39:41 -0500

    public folder middleware

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
 
 // Start the server
 app.listen(port, () => {
