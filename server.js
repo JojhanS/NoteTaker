195054e226bc3c9f2e632b2010a7e9cae4f4ee00
Jojhan Silva <silvajojhan@gmail.com>
2023-07-18 14:52:46 -0500

    added db middleware

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const port = 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Uses public folder
 app.use(express.static('public'));
+
+// Path to the JSON database file
+const dbPath = path.join(__dirname, 'db', 'db.json');
+
 
 // Start the server
 app.listen(port, () => {
