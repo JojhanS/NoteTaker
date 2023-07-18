c7dce726b6243b5ab2db256125d1754b081762ff
Jojhan Silva <silvajojhan@gmail.com>
2023-07-18 15:53:10 -0500

    port fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs')
 const path = require('path')
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
